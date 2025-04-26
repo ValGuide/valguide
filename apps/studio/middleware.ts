74c0b7674622a1253af3e2bc0e3efcf0710f0f5a
Valerius <valerius@valguide.com>
2025-04-27 01:23:04 +0200

    feat(clerk): add clerk to studio

--- apps/studio/middleware.ts
+++ apps/studio/middleware.ts
@@ -2,15 +2,16 @@ import type { NextMiddleware, NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 import { createLogger } from '@valguide/logger'
 
+import createMiddleware from 'next-intl/middleware'
+
 import { hasPathnameLocale, resolveLocale, setLocaleCookie } from '@/i18n/resolve-locale'
-import { auth as authMiddleware } from '@/app/[locale]/(auth)/auth'
 import { isInternalRoute, isLoginRoute, isProtectedRoute, routes, unlocalizedPathname } from '@/routes/routes'
+import { supportedLocales } from '@/i18n/i18n.config'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const log = createLogger('middleware')
 
 const authEnabled = process.env.AUTH_ENABLED !== 'false'
-
-export type AuthMiddlware = typeof authMiddleware
 
 const redirectLocalizedIfRequired = (req: NextRequest, locale: string) => {
   if (!hasPathnameLocale(req)) {
@@ -24,54 +25,51 @@ const redirectLocalizedIfRequired = (req: NextRequest, locale: string) => {
   return NextResponse.next()
 }
 
-// TODO: add auth and tests for it
-export const middlewareFn =
-  (auth: AuthMiddlware): NextMiddleware =>
-  async (req) => {
-    const { pathname } = req.nextUrl
-    const locale = resolveLocale(req)
+// const isProtectedRoute = createRouteMatcher()
 
-    if (authEnabled) {
-      // TODO: simplify
-      const isProtected = isProtectedRoute(pathname)
-      const isInternal = isInternalRoute(pathname)
-      const isLogin = isLoginRoute(pathname)
+export const middleware = clerkMiddleware(async (auth, req) => {
+  const { pathname } = req.nextUrl
+  const locale = resolveLocale(req)
 
-      if (isProtected || isInternal || isLogin) {
-        const session = await auth()
+  if (authEnabled) {
+    // TODO: simplify
+    const isProtected = isProtectedRoute(pathname)
+    const isInternal = isInternalRoute(pathname)
+    const isLogin = isLoginRoute(pathname)
 
-        // we are already logged in and tried to open a login URL
-        if (session && isLogin) {
-          const defaultRedirect = new URL(`/${locale}${routes.console}`, req.url)
-          return NextResponse.redirect(defaultRedirect)
-        }
+    if (isProtected || isInternal || isLogin) {
+      const session = await auth.protect()
 
-        // we are trying to access an internal route without being an internal user
-        if (
-          session &&
-          isInternal &&
-          (!session.user?.email || ['console@valguide.com', 'valerius@valguide.com'].includes(session.user.email))
-        ) {
-          const notFound = new URL(`/${locale}/404`, req.url)
-          return NextResponse.rewrite(notFound)
-        }
+      // we are already logged in and tried to open a login URL
+      if (session && isLogin) {
+        const defaultRedirect = new URL(`/${locale}${routes.console}`, req.url)
+        return NextResponse.redirect(defaultRedirect)
+      }
 
-        // we are trying to access a protected route without being logged in
-        if (!session && !isLogin) {
-          const next = `${unlocalizedPathname(pathname)}${req.nextUrl.search}`
-          const redirectUrl = new URL(`/${locale}${routes.login}`, req.url)
-          redirectUrl.searchParams.set('next', next)
-          return NextResponse.redirect(redirectUrl)
-        }
+      // // we are trying to access an internal route without being an internal user
+      // if (
+      //   session &&
+      //   isInternal &&
+      //   (!session.user?.email || ['console@valguide.com', 'valerius@valguide.com'].includes(session.user.email))
+      // ) {
+      //   const notFound = new URL(`/${locale}/404`, req.url)
+      //   return NextResponse.rewrite(notFound)
+      // }
+
+      // we are trying to access a protected route without being logged in
+      if (!session && !isLogin) {
+        const next = `${unlocalizedPathname(pathname)}${req.nextUrl.search}`
+        const redirectUrl = new URL(`/${locale}${routes.login}`, req.url)
+        redirectUrl.searchParams.set('next', next)
+        return NextResponse.redirect(redirectUrl)
       }
     }
-
-    const res = redirectLocalizedIfRequired(req, locale)
-    setLocaleCookie(res, locale)
-    return res
   }
 
-export const middleware = middlewareFn(authMiddleware)
+  const res = redirectLocalizedIfRequired(req, locale)
+  setLocaleCookie(res, locale)
+  return res
+})
 
 // Read more: https://next-auth.js.org/tutorials/securing-pages-and-api-routes
 export const config = {
