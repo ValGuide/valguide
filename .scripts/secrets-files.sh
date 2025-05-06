1f084494087481debe7c1968168c7eba753541ca
Valerius <valerius@valguide.com>
2025-05-06 13:24:49 +0200

    chore(secrets): hide certificates

--- .scripts/secrets-files.sh
+++ .scripts/secrets-files.sh
@@ -6,4 +6,12 @@ files=(
   ".secrets/.env.supabase.dev"
   ".secrets/.env.supabase.local"
   ".secrets/.env.supabase.prod"
+  "apps/links/certificates/localhost.pem"
+  "apps/links/certificates/localhost-key.pem"
+  "apps/site/certificates/localhost.pem"
+  "apps/site/certificates/localhost-key.pem"
+  "apps/studio/certificates/localhost.pem"
+  "apps/studio/certificates/localhost-key.pem"
+  "apps/visit/certificates/localhost.pem"
+  "apps/visit/certificates/localhost-key.pem"
 )
