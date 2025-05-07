261fdbdfef164b181262f7064085c7760c63bcfa
Valerius <valerius@valguide.com>
2025-05-07 21:36:32 +0200

    feat(theme): add basic theme picker

---
+++ packages/core/ui/components/theme-picker.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { ThemePicker } from './theme-picker'
+
+const meta: Meta<typeof ThemePicker> = {
+  title: 'UI/ThemePicker',
+  component: ThemePicker,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof ThemePicker>
+
+export const Default: Story = {}
+
+export const AlignStart: Story = {
+  args: {
+    align: 'start',
+  },
+}
+
+export const AlignEnd: Story = {
+  args: {
+    align: 'end',
+  },
+}
+
+export const SideTop: Story = {
+  args: {
+    side: 'top',
+  },
+}
+
+export const WithClassName: Story = {
+  args: {
+    className: 'border border-primary p-4 rounded-md',
+  },
+}
