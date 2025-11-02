b2d1916ae17ccab0436e992eab77b1ce7c79fc6d
Valerius <valerius@valguide.com>
2025-11-02 12:10:28 +0100

    feat(studio): add theme switcher

--- apps/studio/app/[locale]/(main)/layout.tsx
+++ apps/studio/app/[locale]/(main)/layout.tsx
@@ -9,6 +9,7 @@ import {
   BreadcrumbSeparator,
 } from '@valguide/ui/components/breadcrumb'
 import { AppSidebar } from '@/components/app-sidebar'
+import { ThemeSwitcher } from '@/components/theme-switcher'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -16,7 +17,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
+          <div className="flex items-center gap-2 px-4 flex-1">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
@@ -30,6 +31,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
+          </div>
+          <div className="flex items-center gap-2 px-4">
+            <ThemeSwitcher />
           </div>
         </header>
         {children}
