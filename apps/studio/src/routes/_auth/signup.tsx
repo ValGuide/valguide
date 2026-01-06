e13c344ebb615b84c381d2a6d10d4913d7118168
Valerius <valerius@valguide.com>
2026-01-06 23:39:28 +0100

    fix(auth): fix auth provider

--- apps/studio/src/routes/_auth/signup.tsx
+++ apps/studio/src/routes/_auth/signup.tsx
@@ -1,9 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { AuthProvider } from '@valguide/features/auth/auth-provider'
 import SignupLoading from '@valguide/features/auth/signup/loading'
-
 import SignupContainer from '@valguide/features/auth/signup/signup-container'
 
 export const Route = createFileRoute('/_auth/signup')({
-  component: SignupContainer,
+  component: () => (
+    <AuthProvider isLogin={false}>
+      <SignupContainer />
+    </AuthProvider>
+  ),
   pendingComponent: SignupLoading,
 })
