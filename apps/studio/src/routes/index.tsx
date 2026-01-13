ba32bdc9678e48e56ffe4a601285de85a53f63f9
Valerius <valerius@valguide.com>
2026-01-13 17:51:58 +0100

    fix(root): redirect / to /guides

--- apps/studio/src/routes/index.tsx
+++ apps/studio/src/routes/index.tsx
@@ -1,10 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-  beforeLoad: ({ context }) => {
-    if (!context.user) {
-      throw redirect({ to: '/login' })
-    }
+  beforeLoad: () => {
     throw redirect({ to: '/guides' })
   },
 })
