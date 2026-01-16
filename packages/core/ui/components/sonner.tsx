204ace48b4c32006ee0ca6f9d75c139fd5156b0b
Valerius <valerius@valguide.com>
2026-01-16 17:11:33 +0100

    feat(sonner): update sonner

--- packages/core/ui/components/sonner.tsx
+++ packages/core/ui/components/sonner.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner, type ToasterProps } from 'sonner'
 const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
+      position="top-center"
       className="toaster group"
       icons={{
         success: <CircleCheckIcon className="size-4" />,
