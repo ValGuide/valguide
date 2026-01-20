1ce05bb1299d91328163cd9216e8215aeb8a44aa
Valerius <valerius@valguide.com>
2026-01-20 21:58:35 +0100

    fix(route): re-generate routes

--- apps/admin/src/routeTree.gen.ts
+++ apps/admin/src/routeTree.gen.ts
@@ -140,14 +140,14 @@ declare module '@tanstack/react-router' {
     '/_main': {
       id: '/_main'
       path: ''
-      fullPath: ''
+      fullPath: '/'
       preLoaderRoute: typeof MainRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/_auth': {
       id: '/_auth'
       path: ''
-      fullPath: ''
+      fullPath: '/'
       preLoaderRoute: typeof AuthRouteImport
       parentRoute: typeof rootRouteImport
     }
--- apps/app/src/routeTree.gen.ts
+++ apps/app/src/routeTree.gen.ts
@@ -145,7 +145,7 @@ declare module '@tanstack/react-router' {
     '/_auth': {
       id: '/_auth'
       path: ''
-      fullPath: ''
+      fullPath: '/'
       preLoaderRoute: typeof AuthRouteImport
       parentRoute: typeof rootRouteImport
     }
