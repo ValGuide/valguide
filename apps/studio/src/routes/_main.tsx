3a7fb6c07ee5157b6d5e1bcab701e3df0ea5d522
Valerius <valerius@valguide.com>
2026-01-23 15:15:42 +0100

    fix(studio): restore top padding on non-editor pages

--- apps/studio/src/routes/_main.tsx
+++ apps/studio/src/routes/_main.tsx
@@ -40,10 +40,10 @@ function MainLayout() {
       <AppSidebarContainer />
       <SidebarInset>
         {!isEditorRoute && (
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 md:hidden">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
             <div className="flex flex-1 items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
+              <SidebarTrigger className="-ml-1 md:hidden" />
+              <Separator orientation="vertical" className="mr-2 h-4 md:hidden" />
             </div>
           </header>
         )}
