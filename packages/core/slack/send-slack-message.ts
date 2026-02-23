ecfcdc4fe3b9d025b642a184c80143ff01da8f9f
Valerius <valerius@valguide.com>
2026-02-23 12:28:22 +0100

    feat(slack): add logs

--- packages/core/slack/send-slack-message.ts
+++ packages/core/slack/send-slack-message.ts
@@ -5,7 +5,16 @@ import { serverEnv } from '../env/server'
 export type SendSlackMessage = (message: SlackMessage) => Promise<void>
 
 export const postMessage: SendSlackMessage = async (message) => {
-  if (!serverEnv.VALBOT_SLACK_TOKEN) return
-  const client = new WebClient(serverEnv.VALBOT_SLACK_TOKEN)
-  await client.chat.postMessage(message)
+  if (!serverEnv.VALBOT_SLACK_TOKEN) {
+    console.warn('[Slack] VALBOT_SLACK_TOKEN is not set. Skipping Slack notification.')
+    return
+  }
+
+  try {
+    const client = new WebClient(serverEnv.VALBOT_SLACK_TOKEN)
+    console.info('[Slack] Sending message:', message)
+    await client.chat.postMessage(message)
+  } catch (e) {
+    console.error('[Slack] Error sending message:', e)
+  }
 }
