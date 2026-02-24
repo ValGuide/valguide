8f9e200a4fb0dbec763581589ca61e383561b1e0
Valerius <valerius@valguide.com>
2026-02-24 15:27:39 +0100

    chore(secrets): hide all

--- apps/app/src/start.ts
+++ apps/app/src/start.ts
@@ -3,6 +3,8 @@ import { dbRequestMiddleware } from '@valguide/core/features/db-middleware'
 import { errorCatchingMiddleware, globalErrorMiddleware } from '@valguide/core/utils/error-catching-middleware'
 
 export const startInstance = createStart(() => ({
+  // Disable SSR by default
+  defaultSsr: false,
   functionMiddleware: [errorCatchingMiddleware],
   requestMiddleware: [dbRequestMiddleware, globalErrorMiddleware],
 }))
--- apps/storybook/.storybook/__mocks__/env-client.ts
+++ apps/storybook/.storybook/__mocks__/env-client.ts
@@ -12,5 +12,5 @@ export const clientEnv: StringifyValues<ClientEnv> = {
   VITE_IMAGEKIT_URL: 'https://ik.imagekit.io/valguide',
   VITE_ENV: 'dev',
   VITE_STUDIO_SUPPORT_EMAIL: 'support@valguide.com',
-  VITE_R2_PUBLIC_URL: 'https://assets.valguide.app',
+  VITE_R2_PUBLIC_URL: 'https://assets.valguide.dev',
 }
