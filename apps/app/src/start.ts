959ba8fefd57ea2d24431eb04de0ac07103106c8
Valerius <valerius@valguide.com>
2026-02-24 15:31:43 +0100

    fix(ssr): change to defaultSsr false for app

--- apps/app/src/start.ts
+++ apps/app/src/start.ts
@@ -3,7 +3,6 @@ import { dbRequestMiddleware } from '@valguide/core/features/db-middleware'
 import { errorCatchingMiddleware, globalErrorMiddleware } from '@valguide/core/utils/error-catching-middleware'
 
 export const startInstance = createStart(() => ({
-  // Disable SSR by default
   defaultSsr: false,
   functionMiddleware: [errorCatchingMiddleware],
   requestMiddleware: [dbRequestMiddleware, globalErrorMiddleware],
