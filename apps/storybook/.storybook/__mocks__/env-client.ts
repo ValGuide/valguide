e1c0698922967bbb2ec044d4ca4012de837a7dfc
Valerius <valerius@valguide.com>
2026-04-21 17:20:33 +0200

    fix(typecheck): align storybook mocks and seed asset types

--- apps/storybook/.storybook/__mocks__/env-client.ts
+++ apps/storybook/.storybook/__mocks__/env-client.ts
@@ -6,10 +6,13 @@ import type { StringifyValues } from '@valguide/core/utils/types'
 export const clientEnv: StringifyValues<ClientEnv> = {
   VITE_POSTHOG_ENABLED: 'false',
   VITE_STUDIO_URL: 'https://studio.valguide.com',
+  VITE_PRIVACY_POLICY_URL: 'https://www.valguide.com/privacy-policy',
+  VITE_TERMS_OF_SERVICE_URL: 'https://www.valguide.com/terms-of-service',
   VITE_APP_DOMAIN: 'valguide.com',
   VITE_IMAGEKIT_URL: 'https://ik.imagekit.io/valguide',
   VITE_ENV: 'dev',
   VITE_STUDIO_SUPPORT_EMAIL: 'support@valguide.com',
+  VITE_ASSET_BASE_URL: '',
   VITE_R2_PUBLIC_URL: 'https://assets.valguide.dev',
   VITE_IMAGE_PROVIDER: 'imagekit',
 }
